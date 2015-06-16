d0f01882339ea1eb0fb822d8276569bcca6434b3
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-06-16 03:30:00 +0200

    Worked some on the RegisterTransaction screen

--- src/application/MainScreenController.java
+++ src/application/MainScreenController.java
@@ -1,11 +1,39 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class MainScreenController {
 
 	@FXML
-	private void registerTransaction(){
+	private Label title;
+	@FXML
+	private AnchorPane centerPanel;
+	@FXML
+	private ScrollPane rightPanel;
+	@FXML
+	private GridPane bottomPanel;
+	
+	private AnchorPane registerTransactionScreen;
+	
+	public MainScreenController(){
+		try {
+			registerTransactionScreen = FXMLLoader.load(getClass().getResource("RegisterTransaction.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		System.out.println("derp");
+	}
+	
+	@FXML
+	private void registerTransaction(){
+		centerPanel.getChildren().setAll(registerTransactionScreen);
 	}
 }
