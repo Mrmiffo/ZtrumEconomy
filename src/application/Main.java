59db0ea2f02b6ecf38b14e73f91a1f0ea4582d1c
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-06-16 03:28:42 +0200

    Renamed Main screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
+			AnchorPane root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
