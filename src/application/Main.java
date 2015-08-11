4a8541f47d593abb9384b6fc15d0faac59260996
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-08-11 02:13:42 +0200

    Added test data

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,7 +1,12 @@
 package application;
 	
+import java.util.ArrayList;
+import java.util.List;
+
+import bookkeeping.AccountType;
+import bookkeeping.Book;
+import bookkeeping.InvalidAccountException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -10,8 +15,20 @@ import javafx.scene.layout.AnchorPane;
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		// TODO remove test data.
+		List<Book> toLaunch = new ArrayList<Book>();
+		Book testBook = new Book("Test");
 		try {
-			AnchorPane root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+			testBook.addAccount("Test acc 1", 1, AccountType.CREDIT);
+			testBook.addAccount("Test acc 2", 2, AccountType.EXPENSE);
+		} catch (InvalidAccountException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		toLaunch.add(testBook);
+		try {
+			AnchorPane root = new MainScreenController(toLaunch);
 			Scene scene = new Scene(root,800,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
