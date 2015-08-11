b9699ccfa65ea4f84e3dff3850f0938b0e51fc5d
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-08-11 02:15:44 +0200

    Added exception for when the book is not correctly selected

---
+++ src/application/NoSuchBookException.java
@@ -0,0 +1,13 @@
+package application;
+
+public class NoSuchBookException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public NoSuchBookException(String error){
+		super(error);
+	}
+}
