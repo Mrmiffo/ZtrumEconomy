6035c9ef3e2c6daccd1dd3d0c582552074514161
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-08-11 02:15:01 +0200

    Setup Account drop down

--- src/application/MainScreenController.java
+++ src/application/MainScreenController.java
@@ -1,18 +1,24 @@
 package application;
 
+import java.io.IOException;
 import java.util.List;
 
 import bookkeeping.Book;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class MainScreenController {
+public class MainScreenController extends AnchorPane {
 
 	//Panels
 	@FXML
@@ -37,24 +43,82 @@ public class MainScreenController {
 	private Button settingsButton;
 	
 	@FXML
-	private MenuButton bookMenu;
-	
-	public MainScreenController(){
+	private ChoiceBox<Book> bookMenu;
 
-	}
 	
 	public MainScreenController(List<Book> books){
-		
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+        try {
+        	fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        bookMenu.getItems().addAll(books);
+        bookMenu.getSelectionModel().selectedIndexProperty().addListener(
+    		new ChangeListener<Number>() {
+            public void changed(ObservableValue ov, Number value, Number new_value) {
+            	try {
+					updateAllScreensWithBook(getSelectedBook());
+				} catch (NoSuchBookException e) {
+					// TODO Auto-generated catch block
+					System.out.println(e.getMessage());
+				}
+            }});       
+        
+	}
+	
+	public MainScreenController(List<Book> books, Book selectedBook){
+		this(books);
+		setSelectedBook(selectedBook);
 	}
 	
 	public void initialize(){
-		registerButton();
+//		registerButton();
 	}
 	
+	public void setSelectedBook(Book book) {
+		bookMenu.getSelectionModel().select(book);
+	}
+	
+	/**
+	 * Used by the BookMenu to update all screens with the new book.
+	 * @param book
+	 */
+	private void updateAllScreensWithBook(Book book){
+		if (bookMenu.getItems().contains(book)){
+            if (centerPanel.getChildren().size() != 0){
+            	try {
+            		for (Node screen: centerPanel.getChildren())
+					((AbstractCenterScreen)screen).setBook(getSelectedBook());
+				} catch (NoSuchBookException e) {
+					// TODO Error if no book exist.
+					System.out.println(e.getMessage());
+				}
+            }
+			
+		}
+	}
+	
+	private Book getSelectedBook() throws NoSuchBookException{
+		if (bookMenu.getSelectionModel().getSelectedIndex() == -1){
+			throw new NoSuchBookException("No book selected");
+		}
+		return bookMenu.getItems().get(bookMenu.getSelectionModel().getSelectedIndex());
+	}
+
 	@FXML
 	private void registerButton(){
-		ScrollPane registerTransactionScreen = new RegisterTransaction();
-		centerPanel.getChildren().setAll(registerTransactionScreen);
+		ScrollPane registerTransactionScreen;
+		try {
+			registerTransactionScreen = new RegisterTransaction(getSelectedBook());
+			centerPanel.getChildren().setAll(registerTransactionScreen);
+		} catch (NoSuchBookException e) {
+			// TODO Add error message
+			e.printStackTrace();
+		}
+		
 	}
 	
 	@FXML
@@ -70,5 +134,10 @@ public class MainScreenController {
 	@FXML
 	private void settingsButton(){
 		
+	}
+	
+	@FXML
+	private void menuButtonSelection(){
+
 	}
 }
