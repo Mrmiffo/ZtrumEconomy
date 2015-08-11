57a4cc1cab0a41e36419be41e179026ddc7947a2
MrMiffo <Marvin.Ztrubas@gmail.com>
2015-08-12 01:25:51 +0200

    Added exception for missing date

---
+++ src/application/NoDateSelectedException.java
@@ -0,0 +1,12 @@
+package application;
+
+public class NoDateSelectedException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoDateSelectedException(String error) {
+		super(error);
+	}
+}
